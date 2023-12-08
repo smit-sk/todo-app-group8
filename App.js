78c7eb7045854dd93898004ae9d934ad2d2d20d6
smit-sk <patelsmit7008@gmail.com>
2023-12-07 22:41:43 -0500

    User Interface

--- App.js
+++ App.js
@@ -1,11 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import TodoItem from './todoItem';
+import { useState } from 'react';
 
 export default function App() {
+  const [tasks, setTasks] = useState([
+    { id: '1', name: 'Task 1', isComplete: false },
+    { id: '2', name: 'Task 2', isComplete: true },
+  ]);
+
+  const [taskName, setTaskName] = useState('');
+
+  const onPressAdd = () => {
+    if (taskName) {
+      const addTask = {
+        id: (tasks.length + 1).toString(),
+        name: taskName,
+        isComplete: false,
+      };
+
+      setTasks((oldTasks) => [...oldTasks, addTask]);
+     
+    }
+    setTaskName('')
+  };
+
+  const onDelete = () => {
+    setTasks([]);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.appBar}>
+        <Text style={styles.appBarText}>
+          Todo List
+        </Text>
+        <MaterialCommunityIcons.Button onPress={onDelete} backgroundColor="white" name="delete" size={24} color="red" />
+      </View>
+      <View style={styles.itemsView}>
+        {tasks.length > 0 ? (
+          <FlatList
+            data={tasks}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <TodoItem item={item} />}
+          />
+        ) : (
+          <Text style={{textAlign:'center'}}>No tasks added</Text>
+        )}
+      </View>
+      <View style={styles.addtaskView}>
+        <TextInput placeholder='Enter a todo task' onChangeText={(text) => setTaskName(text)} style={styles.textInput}></TextInput>
+        <TouchableOpacity onPress={onPressAdd} style={styles.addTaskBtn}>
+          <Text style={styles.addTaskText}> Add Task</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -14,7 +63,45 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginTop: 70,
+    flexDirection: 'column'
+  },
+  appBar: {
+    display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 12
+  },
+  appBarText: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  itemsView: {
+    flex: 1,
     justifyContent: 'center',
   },
+  addtaskView: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingHorizontal: 12,
+    paddingBottom: 40,
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 8
+  },
+  addTaskText: {
+    margin: 8,
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: "bold"
+  },
+  addTaskBtn: {
+    marginLeft: 10,
+    backgroundColor: '#008080'
+  }
 });
---
+++ todoItem.js
@@ -0,0 +1,50 @@
+import { View, Switch, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+
+const TodoItem = ({ item }) => {
+    const [isCompleted, setCompleted] = useState(item.isComplete);
+  
+    const toggleSwitch = () => {
+      setCompleted(!isCompleted);
+    };
+  
+    return (
+      <View style={styles.container}>
+        <View style={styles.itemCon}>
+        <Text style = {{fontWeight:'600',fontSize:18 }}>{item.name}</Text>
+        <Text style={{color: isCompleted ? "green" : 'red' , fontWeight:'500'}}> {isCompleted == true ? "DONE" : "PENDING"} </Text>
+        </View>
+        <Switch
+          value={isCompleted}
+          onValueChange={toggleSwitch}
+        />
+      </View>
+    );
+  };
+  
+
+export default TodoItem;
+
+
+const styles = StyleSheet.create({
+    container:{
+        display:'flex',
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        alignSelf:'stretch',
+        marginHorizontal:10,
+        marginVertical:10,
+        
+    },
+    itemCon:{
+        display:'flex',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignSelf:'flex-start'
+
+
+    }
+  });
+  
+
