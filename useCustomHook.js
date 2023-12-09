663670049f84eac83b61004d13912d88fa260227
Smera-Sanu <s_poovathingalsanu@fanshaweonline.ca>
2023-12-09 15:53:29 -0500

    Created useCustomHook
    This hook is used to change the state of a task 's toggle button.

---
+++ useCustomHook.js
@@ -0,0 +1,16 @@
+
+import { useState } from 'react';
+
+const useCustomHook = (initialValue) => {
+  const [todoCompletionValue, setTodoCompletionValue] = useState(initialValue);
+
+  const toggle = (todoId) => {
+ 
+    todos[todoId].isComplete = !todos[todoId].isComplete;
+    setTodoCompletionValue((prevValue) => !prevValue);
+  };
+
+  return [todoCompletionValue, toggle];
+};
+
+export default useCustomHook;
