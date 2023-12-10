6f5f5f55b6325ae2d7cdcd511af89f75091902ca
dynacorp <olophaayomide@gmail.com>
2023-12-10 18:36:35 -0500

    updated task

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import App from "../App";
+import useTodoViewModel from "../todoViewModel";
 
 describe("App component", () => {
   it("On initial load, message shows that there are no tasks", () => {
@@ -35,7 +36,7 @@ describe("After calling deleteAll(), there should be no more items in the list."
   });
 });
 
-describe("After adding a todo to the list, The todos isComplete property should be set to false", () => {
+describe("changeStatus can be used to mark a task as complete", () => {
   it("should set the todos isComplete property to false", async () => {
     const { getByPlaceholderText, getByText, findByText } = render(<App />);
     const input = getByPlaceholderText("Enter a todo task");
@@ -48,5 +49,22 @@ describe("After adding a todo to the list, The todos isComplete property should 
     fireEvent.press(todoItem);
     const completedTask = findByText("New Task");
     expect(completedTask).toBeTruthy();
+  });
+
+  it("changeStatus can be used to mark a task as complete", () => {
+    const taskId = addTask("new task");
+    expect(todoList[0].isComplete).toBe(false);
+    changeStatus(taskId, true);
+    expect(todoList[0].isComplete).toBe(true);
+    changeStatus(taskId, false);
+    expect(todoList[0].isComplete).toBe(false);
+  });
+});
+
+describe("Add a task", () => {
+  it("should return the id of the added todo", () => {
+    const { result } = renderHook(() => useTodoViewModel());
+    const newId = result.current.addTask("New Task");
+    expect(newId).toBe(0);
   });
 });
--- __tests__/useCustomHook.test.js
+++ __tests__/useCustomHook.test.js
@@ -8,14 +8,11 @@ describe("The custom hook can set the initial value of the todoCompletionValue s
   });
 });
 
-describe("The custom hook can update the todoCompletionValue state variable", () => {
-  it("should update the todoCompletionValue state variable", () => {
-    const initialValue = false;
-    const { result } = renderHook(() => useCustomHook(initialValue));
-    expect(result.current[0]).toBe(initialValue);
-    act(() => {
-      result.current[1]();
-    });
-    expect(result.current[0]).toBe(!initialValue);
+describe("The custom hook can update the state variable", () => {
+  it("The toggle() function updates the state variable", () => {
+    const taskId = addTask("new task");
+    todoList[taskId].isComplete = false;
+    toggle(taskId);
+    expect(todoList[0].isComplete).toBe(true);
   });
 });
